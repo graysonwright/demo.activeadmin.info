be0cc99d57405a262bb7aa382925041861d2176c
Justin Kenyon <kenyonj@gmail.com>
2015-02-03 16:41:30 -0500

    Updates the products index page

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -2,6 +2,17 @@ ActiveAdmin.register Product do
   menu :priority => 2
   permit_params :title, :description,:author,:price, :featured, :available_on,:image_file_name
 
+  index do
+    column :image_path do |product|
+      image_tag product.image_path
+    end
+
+    column :title do |product|
+      link_to product.title, admin_product_path(product)
+    end
+
+    column :author
+  end
   # note: the Product model has a computed property `image_path`
   # that has the correct path for use in the UI
 end
